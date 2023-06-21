785bd38ca58987d7e13a13e61c7fcdba272ec03c
Gifaldy Azka <falcxxdev@pm.me>
2023-06-21 16:25:28 +0700

    feat(ui): add loading bar & page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,7 @@ useHead({
 </script>
 
 <template>
+    <NuxtLoadingIndicator color="var(--gradient)" :throttle="0" :height="3" />
     <div class="container max-w-full font-lexend px-10 lg:px-20 py-10">
         <slot />
     </div>
@@ -16,5 +17,14 @@ useHead({
 @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap');
 :root {
     --gradient: repeating-linear-gradient(90deg, #00dc82 0, #34cdfe 50%, #0047e1);
+}
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
 }
 </style>
