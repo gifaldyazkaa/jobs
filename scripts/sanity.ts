9ba319c2698212d713603113a021f306ed8be298
Gifaldy Azka <falcxxdev@pm.me>
2023-06-21 10:01:24 +0700

    fix(composable): remove _useSanity function

--- scripts/sanity.ts
+++
@@ -1,6 +0,0 @@
-export const _useSanity = async (schema: string, pure?: boolean) => {
-    const sanity = useSanity();
-    const query = pure ? groq`*[_type == "${schema}"]` : groq`*[_type == "${schema}"][0]`;
-    const data: any = await useAsyncData(`${schema}`, () => sanity.fetch(query));
-    return data;
-};
